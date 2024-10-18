fa31ac5cd9fe48cd4e72b60cb1beaac311cd5904
sajneetkaur <sajneet1234.be21@chitkara.edu.in>
2024-10-18 14:17:45 +0530

    add file

---
+++ stringmethod.java
@@ -0,0 +1,18 @@
+ public class stringmethod{
+     public static void main(String[] arrgs){
+        String str="sajneet kaur";
+        System.out.println(str.length());
+        System.out.println(str.charAt(2));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(1,4));
+        System.out.println(str.contains("jn"));
+        System.out.println(str.indexOf("n"));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.replace('e','s'));
+        System.out.println(str.split(" "));
+        
+
+
+}
+}
